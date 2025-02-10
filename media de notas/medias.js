8732b7dacb11af8387b8c1210efb21c1d8e358eb
Nicholas Millani <nicholas.millani@sou.inteli.edu.br>
2025-02-10 11:56:49 -0300

    Add files via upload

---
+++ media de notas/medias.js
@@ -0,0 +1,29 @@
+var n1 = 2.5
+
+var n2 = 10
+
+var n3 = 7
+
+
+var media = (n1 + n2 + n3)/3
+
+console.log("nota 1 : 2.5")
+console.log("nota 2 : 10")
+console.log("nota 3 : 7")
+    
+
+
+
+
+if(media >= 7) {
+    console.log("sua média é" + media + "parabéns você foi aprovado!!");
+}
+else if (media < 7 && media > 4) {
+    console.log("sua média é " + media +" você está de recuperação");
+}
+
+else{
+    console.log("sua média é " + media + " você está reprovado");
+}
+
+
